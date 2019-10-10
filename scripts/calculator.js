2e8e2b47c801f86e55c0cb5963b1030448d69d73
lejonu <leonardo.ferax@gmail.com>
2019-10-10 11:29:14 -0300

    Data e Hora - setInterval e setTimout

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,1 +1,10 @@
-let calculator = new CalcController();
+window.calculator = new CalcController();
+
+// Data e hora
+// calculator.displayDate = new Date().toLocaleString("pt-BR");
+
+// Somente data
+// calculator.displayDate = new Date().toLocaleDateString("pt-BR");
+
+// Somente hora
+// calculator.displayDate = new Date().toLocaleTimeString("pt-BR");
--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,41 +2,91 @@ class CalcController
 {
     constructor()
     {
-        this._displayCalc = 0;
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate;
+
         // inicializa automaticamente através do construtor
         this.initialize();
     }
 
     initialize()
     {
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+        this.setDisplayDateTime();
 
-        displayCalcEl.innerHTML = 1234;
-        dateEl.innerHTML = 1234;
-        timeEl.innerHTML = 1234;
+        setInterval(()=>
+        {
+            this.setDisplayDateTime();
+        }, 1000);
+
+        // Intervalo com timeout
+        // let interval = setInterval(()=>
+        // {
+        //     this.setDisplayDateTime();
+        // }, 1000);
+
+        // setTimeout(() => {
+        //     clearInterval(interval);
+        // }, 5000);
+    }
+
+    setDisplayDateTime()
+    {
+        this.displayTime = new Date().toLocaleTimeString(this._locale);
+        
+        this.displayDate = new Date().toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+
+        });
     }
 
     get displayCalc()
     {
-        return this._displayCalc;
+        // retorna o valor presente no documento
+        return this._displayCalcEl.innerHTML;
     }
 
     set displayCalc(value)
     {
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate()
     {
-        return this._currentDate;
+        return new Date();
     }
 
     set currentDate(value)
     {
         this._currentDate = value;
     }
+
+    get displayTime()
+    {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value)
+    {
+        this._timeEl.innerHTML = value;
+    }
+
+
+    get displayDate()
+    {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value)
+    {
+        this._dateEl.innerHTML = value;
+    }
+
+
+
 
 }
